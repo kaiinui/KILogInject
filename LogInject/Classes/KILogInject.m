4f88873e8f016422bf2eeeec5a5d26bd9f522c09
Kai INUI <lied.der.optik@gmail.com>
2014-08-21 18:29:52 +0900

    improve log format

--- LogInject/Classes/KILogInject.m
+++ LogInject/Classes/KILogInject.m
@@ -4,24 +4,30 @@
 @implementation KILogInject
 
 + (void)inspect:(SEL)selector of:(NSObject *)object {
-    id beforeBlock = [self beforeBlock:selector];
-    id afterBlock = [self afterBlock:selector];
-    [object aspect_hookSelector:selector withOptions:AspectPositionBefore usingBlock:beforeBlock error:NULL];
-    [object aspect_hookSelector:selector withOptions:AspectPositionAfter usingBlock:afterBlock error:NULL];
+    id insteadBlock = [self insteadBlock:selector];
+    [object aspect_hookSelector:selector withOptions:AspectPositionInstead usingBlock:insteadBlock error:NULL];
 }
 
 + (void)inspectInstanceMethod:(SEL)selector ofClass:(Class)klass {
-    id insteadBlock = ^(id<AspectInfo> aspectInfo) {
-        [self logBeforeInvoke:aspectInfo ofSelector:selector];
-        NSDate *start = [NSDate date];
-        void *result = [self invoke:aspectInfo.originalInvocation];
-        NSTimeInterval interval = [[NSDate date] timeIntervalSinceDate:start];
-        NSLog(@"\n\u21E0 %@ %@ in %.5f s", result ,[self methodDescription:aspectInfo withSelector:selector], interval);
-    };
+    id insteadBlock = [self insteadBlock:selector];
     [klass aspect_hookSelector:selector withOptions:AspectPositionInstead usingBlock:insteadBlock error:NULL];
 }
 
 #pragma mark - Helpers
+
++ (id)insteadBlock:(SEL)selector {
+    return ^(id<AspectInfo> aspectInfo) {
+        [self logBeforeInvoke:aspectInfo ofSelector:selector];
+        NSDate *start = [NSDate date];
+        void *result = [self invoke:aspectInfo.originalInvocation];
+        NSTimeInterval milliInterval = [self intervalInMilliSecond:start];
+        [self logAfterInvoke:aspectInfo ofSelector:selector withResult:result withInterval:milliInterval];
+    };
+}
+
++ (NSTimeInterval)intervalInMilliSecond:(NSDate *)startTime {
+    return [startTime timeIntervalSinceNow] * -1000.0;
+}
 
 + (void *)invoke:(NSInvocation *)invocation {
     [invocation invoke];
@@ -31,28 +37,12 @@
     return result;
 }
 
-+ (NSString *)methodDescription:(id<AspectInfo>)aspectInfo withSelector:(SEL)selector {
-    return [NSString stringWithFormat:@"%@-%s", aspectInfo.instance, sel_getName(selector)];
-}
-
-+ (id)beforeBlock:(SEL)selector {
-    return ^(id<AspectInfo> aspectInfo) {
-        [self logBeforeInvoke:aspectInfo ofSelector:selector];
-    };
-}
-
-+ (id)afterBlock:(SEL)selector {
-    return ^(id<AspectInfo> aspectInfo) {
-        [self logAfterInvoke:aspectInfo ofSelector:selector];
-    };
-}
-
 + (void)logBeforeInvoke:(id<AspectInfo>)aspectInfo ofSelector:(SEL)selector {
-    NSLog(@"\n\u21E2 %@ with %@", [self methodDescription:aspectInfo withSelector:selector], aspectInfo.arguments);
+    NSLog(@"\n%@ \u21E2 -%s%@", aspectInfo.instance, sel_getName(selector) , aspectInfo.arguments);
 }
 
-+ (void)logAfterInvoke:(id<AspectInfo>)aspectInfo ofSelector:(SEL)selector{
-    NSLog(@"\n\u21E0 %@", [self methodDescription:aspectInfo withSelector:selector]); // WANT INSPECT RETURN VALUE!
++ (void)logAfterInvoke:(id<AspectInfo>)aspectInfo ofSelector:(SEL)selector withResult:(void *)result withInterval:(NSTimeInterval)milliInterval{
+    NSLog(@"\n%@ \u21E0 -%s [%.2fms] = %@", aspectInfo.instance, sel_getName(selector) , milliInterval, result);
 }
 
 @end
