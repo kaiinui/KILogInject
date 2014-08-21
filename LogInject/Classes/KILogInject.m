c8685a4330e27671ee9c3ac8dea7768d1562dafe
Kai INUI <lied.der.optik@gmail.com>
2014-08-21 15:29:34 +0900

    fix selector name

--- LogInject/Classes/KILogInject.m
+++ LogInject/Classes/KILogInject.m
@@ -4,43 +4,43 @@
 @implementation KILogInject
 
 + (void)inspect:(SEL)selector of:(NSObject *)object {
-    id beforeBlock = [self beforeBlock];
-    id afterBlock = [self afterBlock];
+    id beforeBlock = [self beforeBlock:selector];
+    id afterBlock = [self afterBlock:selector];
     [object aspect_hookSelector:selector withOptions:AspectPositionBefore usingBlock:beforeBlock error:NULL];
     [object aspect_hookSelector:selector withOptions:AspectPositionAfter usingBlock:afterBlock error:NULL];
 }
 
 + (void)inspectInstanceMethod:(SEL)selector ofClass:(Class)klass {
-    id beforeBlock = [self beforeBlock];
-    id afterBlock = [self afterBlock];
+    id beforeBlock = [self beforeBlock:selector];
+    id afterBlock = [self afterBlock:selector];
     [klass aspect_hookSelector:selector withOptions:AspectPositionBefore usingBlock:beforeBlock error:NULL];
     [klass aspect_hookSelector:selector withOptions:AspectPositionAfter usingBlock:afterBlock error:NULL];
 }
 
 #pragma mark - Helpers
 
-+ (NSString *)methodDescription:(id<AspectInfo>)aspectInfo {
-    return [NSString stringWithFormat:@"%@-%s", aspectInfo.instance, sel_getName(aspectInfo.originalInvocation.selector)];
++ (NSString *)methodDescription:(id<AspectInfo>)aspectInfo withSelector:(SEL)selector {
+    return [NSString stringWithFormat:@"%@-%s", aspectInfo.instance, sel_getName(selector)];
 }
 
-+ (id)beforeBlock {
++ (id)beforeBlock:(SEL)selector {
     return ^(id<AspectInfo> aspectInfo) {
-        [self logBeforeInvoke:aspectInfo];
+        [self logBeforeInvoke:aspectInfo ofSelector:selector];
     };
 }
 
-+ (id)afterBlock {
++ (id)afterBlock:(SEL)selector {
     return ^(id<AspectInfo> aspectInfo) {
-        [self logAfterInvoke:aspectInfo];
+        [self logAfterInvoke:aspectInfo ofSelector:selector];
     };
 }
 
-+ (void)logBeforeInvoke:(id<AspectInfo>)aspectInfo {
-    NSLog(@"\n\u21E2 %@ with %@", [self methodDescription:aspectInfo], aspectInfo.arguments);
++ (void)logBeforeInvoke:(id<AspectInfo>)aspectInfo ofSelector:(SEL)selector {
+    NSLog(@"\n\u21E2 %@ with %@", [self methodDescription:aspectInfo withSelector:selector], aspectInfo.arguments);
 }
 
-+ (void)logAfterInvoke:(id<AspectInfo>)aspectInfo {
-    NSLog(@"\n\u21E0 %@", [self methodDescription:aspectInfo]); // WANT INSPECT RETURN VALUE!
++ (void)logAfterInvoke:(id<AspectInfo>)aspectInfo ofSelector:(SEL)selector{
+    NSLog(@"\n\u21E0 %@", [self methodDescription:aspectInfo withSelector:selector]); // WANT INSPECT RETURN VALUE!
 }
 
 @end
